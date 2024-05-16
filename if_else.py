dc44d19c365ca6ec517ee3e0f6487acf7de60d01
Dipendra110 <bhandaridipendra222@gmail.com>
2024-05-16 07:44:01 +0545

    if_else

---
+++ if_else.py
@@ -0,0 +1,48 @@
+# a = 17
+# b = 17
+# if a > b:
+#     print("a is greater than b")
+# elif a == b:
+#     print("a and b are equal")
+# else:
+#     print("b is greater than a")
+#
+#
+# is_rainy=True
+# is_storm=False
+# if is_rainy or is_storm:
+#     print("Stay inside home")
+# else:
+#     print("ENjoy")
+
+
+# amount=10000
+# is_good_credit=True
+# if is_good_credit:
+#     down_payment=(10/100)*amount
+# else:
+#     down_payment=(20/100)*amount
+# print(down_payment)
+
+# speed=int(input("Enter the speed:"))
+# if speed>60:
+#     print("YOur speed limit is high")
+# else:
+#     print("You are on safer mode")
+
+# number=int(input("Enter the number:"))
+# if number%7==0:
+#     print("True")
+# else:
+#     print("False")
+
+
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+c=int(input("Enter the value of c:"))
+if a>b>c:
+    print(f"{a} is the greatest number among all")
+elif b>a>c:
+    print(f"{b} is the greatest number among all")
+else:
+    print(f"{c} is the greatest number among all")
