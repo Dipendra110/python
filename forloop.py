3f8c76ef36af990e9dcbeb0ea825a262b4adf17f
Dipendra110 <bhandaridipendra222@gmail.com>
2024-05-17 07:42:39 +0545

    list_forloop

---
+++ forloop.py
@@ -0,0 +1,17 @@
+# name="Hello"
+# for i in range(len(name)):
+#     print(name[i])
+#     print(i)
+
+# for item in range(5,10):
+#     print(item)
+#     print("a")
+# print("hello")
+
+# for item in range(5,10,3):
+#     print(item)
+price=[20,30,40]
+total=0
+for i in price:
+    total+=i
+print(total)
---
+++ lists.py
@@ -0,0 +1,33 @@
+# a=[3,7,8,4,9]
+# a.append(0)
+# a.insert(2,3)
+# a.remove(9)
+# a.pop()          ##Remove last element of list
+# print(a)
+
+
+# b=[3,2,8,9,4,7,6,]
+# print("Index", b.index(8))
+# print("Count", b.count(9))
+# b.sort()
+# print("Sorted", b)
+# b.reverse()
+# print("Reversed", b)
+# print("Length ", len(b))
+
+#Copy creates new objects and only copies the reference
+# fruits=["apple","pear","orange","banana"]
+# c=fruits.copy()
+# c.append("cherry")
+# print(c)
+
+
+# my_list1=[0]*5
+# my_list2=[1,2,3,4,5]
+# my_list3=my_list1+my_list2
+# print(my_list3)
+
+e=[1,2,3,4,5,6,7,8,9]
+print(e[1:5])
+print(e[2:-1])
+print(e[2::2])
