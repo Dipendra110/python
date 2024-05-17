76d4b896be79174916accf4242f3ee4ca9486828
Dipendra110 <bhandaridipendra222@gmail.com>
2024-05-17 07:53:06 +0545

    tuples

--- forloop.py
+++ forloop.py
@@ -10,8 +10,13 @@
 
 # for item in range(5,10,3):
 #     print(item)
-price=[20,30,40]
-total=0
-for i in price:
-    total+=i
-print(total)
+# price=[20,30,40]
+# total=0
+# for i in price:
+#     total+=i
+# print(total)
+
+
+e=[1,2,3,4,5,6,7,8,9]
+e.append(10)
+print(e)
