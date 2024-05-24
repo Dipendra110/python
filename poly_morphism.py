db19dc39ec5063d7686a32f7b9f9d2b38c636d87
Dipendra110 <bhandaridipendra222@gmail.com>
2024-05-24 07:19:07 +0545

    Polymorphism

--- poly_morphism.py
+++ poly_morphism.py
@@ -1,26 +1,43 @@
-class Cat:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def info(self):
-        print(f"My cat name is {self.name} and {self.name} is {self.age} years old.")
-    def make_sound(self):
-        print("Meow")
-class Dog:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def info(self):
-        print(f"My dog name is {self.name} and {self.name} is {self.age} years old.")
-    def make_sound(self):
-        print("Bark")
-cat1=Cat("Charlie",2)
-dog1=Dog("Rahul",7)
+# class Cat:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def info(self):
+#         print(f"My cat name is {self.name} and {self.name} is {self.age} years old.")
+#     def make_sound(self):
+#         print("Meow")
+# class Dog:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def info(self):
+#         print(f"My dog name is {self.name} and {self.name} is {self.age} years old.")
+#     def make_sound(self):
+#         print("Bark")
+# cat1=Cat("Charlie",2)
+# dog1=Dog("Rahul",7)
 
 # for animal in (cat1, dog1):
 #     animal.make_sound()
 #     animal.info()
 #     animal.make_sound()
 
-dog1.make_sound()
-cat1.info()
+# dog1.make_sound()
+# cat1.info()
+
+class Nepal:
+    def lang(self):
+        print("Nepali is the language of Nepal")
+    def cap(self):
+        print("Kathmandu is capital city of Nepal")
+
+class India:
+    def lang(self):
+        print("Bhojpuri is the language of India")
+
+    def cap(self):
+        print("Delhi is capital city of India")
+
+a=Nepal()
+a.lang()
+
